47d789aa6d2e274122ec510e3adf3cb2a67c95f9
marwenez <labidimarwen6@gmail.com>
2022-03-15 13:27:07 +0100

    🎉🎉 pwa inject manigest strategy work fine until now

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,12 @@ export default defineConfig({
 	},
 	plugins: [
 		VitePWA({
+			mode: "development",
+			filename: "sw.js",
 			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			strategies: "injectManifest",
+			registerType: 'autoUpdate',
+			injectRegister: "inline",
 			manifest: {
 				name: 'KBAN',
 				short_name: 'KBAN',
@@ -38,11 +43,7 @@ export default defineConfig({
 					}
 				]
 			},
-			strategies: 'injectManifest',
-			registerType: 'autoUpdate',
-			srcDir: 'src/js/',
-			filename: 'sw.js',
-			
+
 		})
 	],
 
